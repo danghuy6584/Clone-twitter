2624808605a0ecb3d03639eb41f7373bc3893620
Van Huy <danghuy6584@gmail.com>
2024-08-27 21:58:40 +0700

    task 1 -  auth routes and controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,15 +1,108 @@
+import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js";
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+
 export const signup = async (req, res) => {
-  res.json({
-    data: "Sign up route",
-  });
+  try {
+    const { username, fullName, email, password } = req.body;
+    console.log(req.body);
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "Invalid email format" });
+    }
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: "Username is already taken" });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ error: "Email is already taken" });
+    }
+
+    if (password.length < 6) {
+      console.log(username, fullName, email, password);
+      return res
+        .status(400)
+        .json({ error: "Password must be at least 6 characters long" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        username: newUser.username,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        followers: newUser.followers,
+        following: newUser.following,
+        profilePicture: newUser.profilePicture,
+        coverImg: newUser.coverImg,
+      });
+    } else {
+      return res.status(400).json({ error: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup", error);
+    res.status(500).json({ error: error.message });
+  }
 };
 export const login = async (req, res) => {
-  res.json({
-    data: "log in route",
-  });
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      username: user.username,
+      fullName: user.fullName,
+      email: user.email,
+      followers: user.followers,
+      following: user.following,
+      profilePicture: user.profilePicture,
+      coverImg: user.coverImg,
+    });
+  } catch (error) {}
 };
 export const logout = async (req, res) => {
-  res.json({
-    data: "log out route",
-  });
+  try {
+    res.cookie("jwt", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout", error);
+    res.status(500).json({ error: error.message });
+  }
+};
+export const getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password");
+    res.status(200).json(user);
+  } catch (error) {
+    console.log("error in getMe controller", error.message);
+    res.status(500).json({ error: "Internal sever error" });
+  }
 };
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,18 @@ import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 import connectMongodb from "./db/connectMongodb.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 console.log(process.env.MONGO_URI);
+
+app.use(express.json()); // for parsing req.body
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
